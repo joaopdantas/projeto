feb5b9b39ea8c269a3178d3f711a3973fb923b98
joaopdantas <joao.dantas@ipvc.pt>
2023-04-14 11:37:47 +0100

    change 1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.projeto.projeto {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.projeto to javafx.fxml;
     exports com.projeto;
