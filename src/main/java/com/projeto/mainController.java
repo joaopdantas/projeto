9eaae560d337e4f6793e43b0d3c5424ddd5357fb
joaopdantas <joao.dantas@ipvc.pt>
2023-04-14 11:42:17 +0100

    change 2

--- src/main/java/com/projeto/mainController.java
+++ src/main/java/com/projeto/mainController.java
@@ -15,36 +15,6 @@ public class mainController {
     @FXML
     private ListView<String> list;
 
-    /*public String getInfo() throws SQLException {
-
-        // Establish a connection to the database
-        Connection conn = DriverManager.getConnection("jdbc:jtds:sqlserver://LAPTOP-5K0DBPI8:1433/projetoei;instance=EI26272;user=sa;password=123456");
-
-        // Create a statement object
-        Statement statement = conn.createStatement();
-
-        // Execute a SQL query
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM CODPOSTAL");
-
-        // Process the result set
-        String cdPostal = null;
-        String name = null;
-        while (resultSet.next()) {
-            cdPostal = resultSet.getString("CODPOSTAL");
-            name = resultSet.getString("LOCALIDADE");
-            System.out.println("ID: " + cdPostal + ", Name: " + name);
-        }
-
-        // Close the resources
-        resultSet.close();
-        statement.close();
-        conn.close();
-
-        return ("ID: " + cdPostal + ", Name: " + name);
-
-    }*/
-
-
     public void initialize() throws SQLException {
         // Establish a connection to the database
         Connection conn = DriverManager.getConnection("jdbc:jtds:sqlserver://DESKTOP-VDIJP2R:1433/projetoei;instance=EI24901;user=sa;password=123456");
@@ -53,15 +23,15 @@ public class mainController {
         Statement statement = conn.createStatement();
 
         // Execute a SQL query
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM CODPOSTAL");
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM CONSULTA");
 
         // Create an ObservableList to store the data
         ObservableList<String> items = FXCollections.observableArrayList();
 
         // Process the result set
         while (resultSet.next()) {
-            String cdPostal = resultSet.getString("CODPOSTAL");
-            String name = resultSet.getString("LOCALIDADE");
+            int cdPostal = resultSet.getInt("ID_CONSULTA");
+            String name = resultSet.getString("DESCRICAO");
             String item = cdPostal + " - " + name;
             items.add(item);
         }
